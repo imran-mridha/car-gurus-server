be46549a5e4944ecfcddb90f6e8b925fddf91c92
imran-mridha <mdimranmridha.pro@gmail.com>
2022-11-24 13:28:04 +0600

    Server Created

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send("Doctors portal server running....");
+});
+
+app.listen(port, () => {
+  console.log("Server Running on port", port);
+});
